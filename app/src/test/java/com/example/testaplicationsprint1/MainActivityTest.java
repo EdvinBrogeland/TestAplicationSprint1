f701f1cdba5a6a5013b30a3917dc27b314b2b1bb
EdvinBrogeland <ebrogeland@gmail.com>
2022-04-06 13:26:24 +0200

    Initial commit

---
+++ app/src/test/java/com/example/testaplicationsprint1/MainActivityTest.java
@@ -0,0 +1,7 @@
+package com.example.testaplicationsprint1;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+}
---
+++ app/src/test/java/com/example/testaplicationsprint1/TestClass.java
@@ -0,0 +1,11 @@
+package com.example.testaplicationsprint1;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class TestClass extends TestCase {
+    @Test
+    public void testSubtraction(){assertEquals(1,4-3);}
+
+}
